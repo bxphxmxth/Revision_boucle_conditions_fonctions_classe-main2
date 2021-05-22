009c6b0777af33a6ee1339bf90f718fd5b44c3e8
bxphxmxth <fushs@hotmail.fr>
2021-05-22 14:39:15 +0200

    forEach

--- src/js/main.js
+++ src/js/main.js
@@ -1059,3 +1059,66 @@ console.log(ritsu);
 // }else{
 //      alert("raté gros nul");
 // }
+
+// forEach
+
+// -  # Exo 1 Boucles FOREACH
+//     ## - Creer une variable de type array et inserez toutes les personnes de la classe
+//     ## - A l'aide d'une boucle foreach, dites bonjour + prenom à chaque personne du tableau
+// let classe = ["Adil","Agim","Ali","Antoine","Chris","Elvis","Fanny","Haroune","Ilias D","Ilias El","Jean","Kevin","Lira","Mouna","Nasila","Nathan","Seif","Stan","Van Hoa","Yassine"];
+
+
+// classe.forEach(element =>{
+//      console.log(`Yo ca gaze ${element} ? :3`);
+// })
+
+
+// - # Exo 2 Boucle FOREACH
+//     - ## Créez un un array qui contient 5 éléments
+//     - ## Affichez touts les éléments a l'aide d'une boucle foreach
+
+// let array = ["j'ai", "faim", "mais", "flemme", "cuisiner"];
+
+// array.forEach(element =>{
+//      alert(element);
+// })
+
+//     ## Triez le contenu du tableau donnees en verifiant le type de chaque donnée
+//     ## Creez 4 Tableaux : 'typeString' , 'typeNumber' , 'typeObject' , 'typeAutre.
+//     ## Videz le tableau "donnees" en redistribuant avec l'aide de condition et d'une function prédéfinie trouvée sur internet dans les bons tableaux.
+let donnees = [14, 7, 97, {}, "247", 67, 101, true, 34, 78, [], 'coding school', 0, 11, 32, "italie", 61, null, 'cent', 100];
+
+let typeString = [];
+let typeNumber = [];
+let typeObject = [];
+let typeAutre = [];
+
+    // 1. je console log le type de chacun de mes éléments
+   // 2. je trie mes éléments en fonction de leur type
+
+
+    donnees.forEach(element =>{
+         console.log(typeof element);
+         if(typeof element == "string"){
+              typeString.push(element);
+         }else if(typeof element == "number"){
+              typeNumber.push(element);
+         }else if(typeof element == "object"){
+              typeObject.push(element);
+         }else{
+              typeAutre.push(element);
+         }
+    });
+
+    console.log(typeString);
+    console.log(typeNumber);
+    console.log(typeObject);
+    console.log(typeAutre);
+ 
+//3. supprime tout ce qu'il y a dans le tableau
+donnees.splice(0, donnees.length);
+
+
+//4. j'affiche tout
+
+console.log(donnees);
